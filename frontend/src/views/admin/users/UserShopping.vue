43eb91168fa7cb6ec6cd9baa2a11ca236d0faef6
SDHCRRFR <houdjadji_said@carrefour.com>
2023-09-13 18:32:16 +0200

    house

--- frontend/src/views/admin/users/UserShopping.vue
+++ frontend/src/views/admin/users/UserShopping.vue
@@ -1,29 +1,57 @@
 <template>
-  <div class="user_shop">
-    <h1>
-      user shop <span>user: {{ id }}</span>
-    </h1>
+  <div class="house" id="house" data-rooms="6">
+    <div class="house-wings" data-flip-key="wings">
+      <div class="house-left-wing">
+        <div class="house-window"></div>
+        <div class="house-window"></div>
+        <div class="house-sparkle">
+          <div class="house-sparkle-dots"></div>
+        </div>
+      </div>
+      <div class="house-right-wing">
+        <div class="house-window"></div>
+        <div class="house-window"></div>
+        <div class="house-sparkle">
+          <div class="house-sparkle-dots"></div>
+        </div>
+      </div>
+      <div class="house-roof">
+        <div class="house-ledge"></div>
+      </div>
+    </div>
+    <div class="house-front" data-flip-key="front">
+      <div class="house-chimney"></div>
+      <div class="house-facade"></div>
+      <div class="house-window">
+        <div class="house-sparkle">
+          <div class="house-sparkle-dots"></div>
+        </div>
+      </div>
+      <div class="house-doorway">
+        <div class="house-stairs"></div>
+        <div class="house-door"></div>
+      </div>
+      <div class="house-gable">
+        <div class="house-roof">
+          <div class="house-ledge"></div>
+        </div>
+      </div>
+    </div>
   </div>
+
+  <label class="house-label" for="range" id="label">Rooms</label>
+  <input type="range" min="3" max="6" step="1" value="6" id="range">
+  
 </template>
+
+
 <script>
 export default {
-  name: 'UserShopping',
-  props: ['id'],
-  data() {
-    return {}
-  }
-  // mounted() {
-  //   console.log(this.$route.params.id)
-  // }
+
 }
 </script>
+
+
 <style scoped>
-.user_shop {
-  width: 100%;
-  padding-top: 100px;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+
 </style>
