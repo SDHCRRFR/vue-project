44c69e036bb9cf56a2112968bf2c087c192845e1
SDHCRRFR <houdjadji_said@carrefour.com>
2023-09-23 18:16:03 +0200

    fr

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -15,47 +15,25 @@ localStorage.setItem('token', 'marcel')
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
+    { path: '/',
       name: 'public',
       component: Public.PublicLayout,
       children: [
         { path: '/', name: 'home', component: Public.HomeView },
         { path: '/shopping-cart', component: Public.ShoppingCart, name: 'ShoppingCart' },
-        { path: '/about', name: 'about', component: () => import('../views/public/AboutView.vue') }
-      ]
-    },
-    { path: '/login', name: 'UserLogin', component: Auth.UserLogin },
-    { path: '/signup', name: 'SignUp', component: Auth.SignUp },
-    {
-      path: '/admin',
+        { path: '/about', name: 'about', component: () => import('../views/public/AboutView.vue')}]},
+    { path: '/admin',
       name: 'admin',
       // beforeEnter: authGuard,
       component: Admin.AdminLayout,
-      children: [
-        { path: 'dashboard', name: 'UserDashboard', component: Admin.UserDashboard },
-        {
-          path: 'user/shop/:id(\\d+)',
-          name: 'user-shopping',
-          component: Admin.UserShopping,
-          props: true
-        },
-        {
-          path: 'user/index/:id(\\d+)',
-          name: 'user-index',
-          component: Admin.UserIndex,
-          props: true
-        },
-        {
-          path: 'user/don/:id(\\d+)',
-          name: 'user-fait-un-don',
-          component: Admin.FaireUnDon,
-          props: true
-        },
+      children: [ { path: 'dashboard', name: 'UserDashboard', component: Admin.UserDashboard },
+        { path: 'user/shop/:id(\\d+)', name: 'user-shopping', component: Admin.UserShopping, props: true },
+        { path: 'user/index/:id(\\d+)', name: 'user-index', component: Admin.UserIndex, props: true },
+        { path: 'user/don/:id(\\d+)', name: 'user-fait-un-don', component: Admin.FaireUnDon, props: true },
         { path: '/admin/user/wishlist', name: 'user-wishlist', component: Admin.WishList },
-        { path: '/admin/logout', name: 'UserLogout', component: Admin.UserLogout }
-      ]
-    },
+        { path: '/admin/logout', name: 'UserLogout', component: Admin.UserLogout }]},
+    { path: '/login', name: 'UserLogin', component: Auth.UserLogin },
+    { path: '/signup', name: 'SignUp', component: Auth.SignUp },
     { path: '/management', name: 'ManagementStore', component: ManagementStore },
     { path: '/:pathMatch(.*)*', component: NotFound }
   ]
