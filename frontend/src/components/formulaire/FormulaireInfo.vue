799fa1e8aca924dbccfd26e9e892a7c840ca2e17
SDHCRRFR <houdjadji_said@carrefour.com>
2023-10-17 13:31:19 +0200

    formulaire/info

--- frontend/src/components/formulaire/FormulaireInfo.vue
+++ frontend/src/components/formulaire/FormulaireInfo.vue
@@ -1,54 +1,51 @@
 <template>
   <div class="formulaire_info">
-    <form action="#" class="form">
+    <form action="#" class="form" @submit.prevent="submit">
       <h1>On en discute ?</h1>
       <p>
         Vous êtes un restaurateur et souhaitez en savoir plus sur Table de coeur ? Remplissez vos
         informations personnelles et un membre de notre équipe reviendra rapidement vers vous.
       </p>
-
       <div class="column">
         <div class="input_box">
-        
           <input type="text" placeholder="Votre Prénom" required />
         </div>
         <div class="input_box">
-         
           <input type="text" placeholder="Votre Prénom" required />
         </div>
       </div>
-
       <div class="input_box">
-        
         <input type="text" placeholder="Entrez votre mail" required />
       </div>
-
      <div class="column">
         <div class="input_box">
-          
           <input type="text" placeholder="Entrez votre numéro" required />
         </div>
-    </div>
+     </div>
     <div class="remember_forgot">
-        <label><input type="checkbox" />
-        </label>
+        <label><input type="checkbox"/></label>
         <p>En cliquant sur "Suivant" j'accepte la Politique de confidentialité de TheFork.</p>
-      </div>
-      <button type="submit">Envoyez</button>
+     </div>
+     <button type="submit">Envoyez</button>
     </form>
     <img src="../../../public/nourriture_salade_o.jpeg" alt="" />
   </div>
 </template>
+
+
 <script>
+
 export default {
   name: 'FormulaireInfo'
+
 }
 </script>
+
+
 <style scoped>
 .formulaire_info {
   width: 100%;
   background: rgba(47, 210, 255, 0.084);
-  /* border-radius: 8px; */
   border: none;
   display: flex;
   flex-direction: row;
@@ -69,10 +66,13 @@ button {
     border-radius: 5px;
     border: none;
     margin-left: 47%;
-    background: rgb(0, 101, 30);
+    background: rgba(0, 101, 30, 0.563);
+    cursor: pointer;
 }
 
-
+button:hover {
+    background: greenyellow;
+}
 
 h1 {
   font-size: 70px;
